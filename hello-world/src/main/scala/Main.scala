23cc58045750723dc3b579138d40ae73e37e7d41
= <steviepike@yahoo.co.uk>
2022-11-18 15:44:24 +0000

    build initial example scala project

---
+++ hello-world/src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+@main def hello: Unit = 
+  println("Hello world!")
+  println(msg)
+  println("Goodbye world!")
+  println("So long, and thanks for all the fish")
+
+def msg = "I was compiled by Scala 3. :)"
